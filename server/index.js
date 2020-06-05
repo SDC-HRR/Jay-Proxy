9b7c9d169696793fdd9d565a7258ee355ee94090
Jay Barr <jayrbarr@gmail.com>
2020-06-04 21:45:49 -0400

    Corrects sending of data from cache

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const cacheCheck = (req, res, next) => {
   client.get(id, (err, check) => {
     console.log(check);
     if (check !== null) {
-      res.status(200).json(check);
+      res.status(200).send(check);
     } else {
       next();
     }
